8e0e7d13c7f880503da344814398646aef125336
eugenepro2 <eugenepro@icloud.com>
2019-01-06 13:17:21 +0200

    upoad

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
-import sayHello from './lib/sayHello.js';
+import 'slick-carousel/slick/slick';
+import $ from 'jquery';
 
-sayHello();
+$('.slick').slick({
+  arrows: false,
+});
+
+let slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
+});
+
+document.querySelector('.header__menu').addEventListener('click', function() {
+  slideout.toggle();
+});
