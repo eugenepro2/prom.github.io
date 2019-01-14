40e449f456710a8d94fba3b2ef31a3ea306d82b8
eugenepro2 <eugenepro@icloud.com>
2019-01-14 10:47:08 +0200

    upload

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,36 @@ $('.slick').slick({
   arrows: false,
 });
 
-let slideout = new Slideout({
-  'panel': document.getElementById('panel'),
-  'menu': document.getElementById('menu'),
-  'padding': 256,
-  'tolerance': 70
-});
 
-document.querySelector('.header__menu').addEventListener('click', function() {
-  slideout.toggle();
-});
+function InitSlideout() {
+  var BUTTON_SELECTOR = '[data-toggle-mobile-menu]';
+
+  var slideout = new Slideout({
+    'panel':     document.getElementById('panel'),
+    'menu':      document.getElementById('menu'),
+    'side':      'left',
+    'padding':   256,
+    'tolerance': 70,
+    'touch':     false
+  });
+
+  $(BUTTON_SELECTOR).on('click', function() {
+    slideout.toggle();
+  });
+
+  $(window).on('resize', function() { 
+    slideout.close(); 
+  });
+
+  slideout.on('close', function() {
+    $(BUTTON_SELECTOR).addClass('is-closing');
+    $(BUTTON_SELECTOR).removeClass('is-active');
+  });
+
+  slideout.on('open', function() {
+    $(BUTTON_SELECTOR).removeClass('is-closing');
+    $(BUTTON_SELECTOR).addClass('is-active');
+  });
+}
+
+InitSlideout();
